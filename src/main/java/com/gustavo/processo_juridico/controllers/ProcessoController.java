4a54a36baef419c298d0560bd03b7117a9046e64
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2025-08-30 18:00:03 -0300

    feat: PessoaController criar processo

---
+++ src/main/java/com/gustavo/processo_juridico/controllers/ProcessoController.java
@@ -0,0 +1,32 @@
+package com.gustavo.processo_juridico.controllers;
+
+import com.gustavo.processo_juridico.dtos.pessoa.CriarPessoaRequest;
+import com.gustavo.processo_juridico.dtos.pessoa.PessoaResponse;
+import com.gustavo.processo_juridico.dtos.processo.CriarProcessoRequest;
+import com.gustavo.processo_juridico.dtos.processo.ProcessoResponse;
+import com.gustavo.processo_juridico.usecases.pessoa.CriarPessoaUseCase;
+import com.gustavo.processo_juridico.usecases.processo.CriarProcessoUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/processos")
+public class ProcessoController {
+    private final CriarProcessoUseCase criarProcessoUseCase;
+
+    public ProcessoController(CriarProcessoUseCase criarProcessoUseCase) {
+        this.criarProcessoUseCase = criarProcessoUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProcessoResponse> criar(@Valid @RequestBody CriarProcessoRequest processoRequest) {
+        ProcessoResponse processoResponse = criarProcessoUseCase.execute(processoRequest);
+        return ResponseEntity.created(URI.create("/api/v1/processos/" + processoResponse.id())).body(processoResponse);
+    }
+}
