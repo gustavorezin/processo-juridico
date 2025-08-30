9f6f72d57ced43942421335192966b89b85686f8
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2025-08-30 14:51:10 -0300

    init: configurações iniciais do projeto

---
+++ src/main/java/com/gustavo/processo_juridico/ProcessoJuridicoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.processo_juridico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoJuridicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoJuridicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/processo_juridico/ProcessoJuridicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.processo_juridico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoJuridicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
